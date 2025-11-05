8e4d4b95b4a17aef19d3eb68c0f4c3c19a11df3b
Brant Robinson <brant.robinson@sdgku.edu>
2025-11-05 14:49:23 -0800

    added coupon endpoints for assignment 3

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from http import HTTPStatus
 
 app = Flask(__name__) # instance of FLask
 
@@ -49,6 +50,90 @@ def get_user():
      return user
 
 
+
+
+
+# Session #3
+# Path Parameters
+# Is a dynamic part of the URL used to identify a specific item or resource
+@app.route("/greet/<string:name>")
+def greet(name):
+     return f"Hey {name}"
+
+student_names = ["Micah", "Brant", "Jeuan", "Mat", "Eric"]
+
+@app.route("/students", methods=["POST"])
+def add_student():
+     student_names.append("Leo")
+     print("Hello Cohort 61")
+     return student_names
+
+products = [
+     {
+          "_id": 1,
+          "title": "Nintendo Switch",
+          "price": 299.99,
+          "category": "electronics",
+          "image": "https://picsum.photos/seed/1/300/300"
+     },
+     {
+          "_id": 2,
+          "title": "Smart Refrigerator",
+          "price": 999.99,
+          "category": "kitchen",
+          "image": "https://picsum.photos/seed/2/300/300"
+     },
+     {
+          "_id": 3,
+          "title": "BLuetooth Speaker",
+          "price": 79.99,
+          "category": "electronics",
+          "image": "https://picsum.photos/seed/3/300/300"
+     }
+]
+
+
+
+@app.route("/api/products")
+def get_products():
+     return jsonify({
+          "success": True,
+          "message": "Products retrieved successfully",
+          "data": products
+     }), HTTPStatus.OK # 200 - it would do this without this code, but we are overriding to always return code 200
+
+
+@app.route("/api/products/<int:product_id>", methods=["GET"])
+def  get_product_by_id(product_id):
+     print(product_id)
+     for product in products:
+          if product["_id"] == product_id:
+               return jsonify({
+                    "success": True,
+                    "message": "Products retrieved successfully",
+                    "data": product
+     })
+     return jsonify({
+          "success": False,
+          "message": "Product not found"
+     }),HTTPStatus.NOT_FOUND
+          
+
+@app.post("/api/products")
+def add_product():
+     new_product = request.get_json()
+     new_product["_id"] = len(products) + 1
+     products.append(new_product)
+     return jsonify ({
+          "success": True,
+          "message": "Product Successfully Added",
+          "data": new_product
+     }), HTTPStatus.CREATED
+
+
+
+
+#-----------COUPONS------------
 coupons = [
         {"_id": 1, "code": "WELCOME10", "discount": 10},
         {"_id": 2, "code": "SPOOKY25", "discount": 25},
@@ -60,11 +145,39 @@ coupons = [
 def get_coupons():
      return jsonify(coupons)
 
+@app.route("/api/coupon/<int:_id>", methods=["GET"])
+def get_one_coupon(_id):
+     for coupon in coupons:
+          if coupon["_id"] == _id:
+               return jsonify({
+                    "success": True,
+                    "message": "Coupon retrieved successfully",
+                    "data": coupon
+     })
+     return jsonify({
+          "success": False,
+          "message": "Coupon not found"
+     }),HTTPStatus.NOT_FOUND
+
+
+@app.post("/api/coupons")
+def add_coupon():
+     new_coupon = request.get_json()
+     new_coupon["_id"] = len(coupons) + 1
+     coupons.append(new_coupon)
+     return jsonify ({
+          "success": True,
+          "message": "Coupon Successfully Added",
+          "data": new_coupon
+     }), HTTPStatus.CREATED
+
 
 @app.route("/api/coupons/count", methods=["GET"])
 def get_coupon_count():
     num_coupons = len(coupons)
-    return jsonify(num_coupons)
+    return jsonify({"coupon-count ": num_coupons})
+
+
 
 
 @app.route("/cohort/<id>", methods=["GET"])
